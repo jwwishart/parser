708cadad452c22dd805662590196b4df37fe8a70
Justin Wishart <jwwishart@gmail.com>
2016-11-18 22:38:15 +1000

    Compiler Command Line... thats it! :o(

--- parser.cpp
+++ parser.cpp
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include "scanner/scanner.h"
 
+// clang++ -std=c++14 -g parser.cpp -o bin/parser
+
 int main() {
     auto context = scanner_init_string((char *)"Hello World");
     char c;
